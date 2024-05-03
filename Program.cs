7e1162fcfb44a2b25d934863e9720301cbc2d91b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 19:53:19 -0300

    ATIVIDADE CONCLUIDA

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numero;
+
+
+Console.WriteLine("----Atividade de PC1----\n");
+Console.Write("Digite um numero:");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero <0)
+{
+    Console.WriteLine("Negativo");
+}
+
+else 
+{
+    if (numero >0)
+    Console.WriteLine("Positivo");
+
+    else 
+    Console.WriteLine("Zero");
+}
